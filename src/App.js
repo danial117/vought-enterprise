4ebce0fa1eb1812253f77d6edfb83d65a7628749
danial117 <danigondal117@gmail.com>
2023-10-29 07:24:18 -0700

    deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Info from './scenes/Info'
 const App=() =>{
     return(
         <div className='app'>
-    <BrowserRouter basename="/voughtproxy">
+    <BrowserRouter basename="/voughtinternationals">
 
     <Routes>
         <Route path={'/'} element={<HomePage/> } />
