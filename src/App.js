ba37cec3814edc93e8c44827060a541a398a06af
danial117 <danigondal117@gmail.com>
2023-10-29 18:19:31 -0700

    ready for deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from './scenes/Navbar'
 const App=() =>{
     return(
         <div className='app'>
-    <BrowserRouter basename="/react-app">
+    <BrowserRouter basename="/vought-internationals">
 
     <Routes>
         <Route path={'/'} element={<Navbar/> } />
